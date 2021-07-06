42e24c5c482429204ef750a8fc98096e06e09af9
kevonh <kevonh@gmail.com>
2021-07-06 15:31:19 -0700

    Simple change

--- DieRoller/Client/obj/Debug/net5.0/RazorDeclaration/Shared/NavMenu.razor.g.cs
+++ DieRoller/Client/obj/Debug/net5.0/RazorDeclaration/Shared/NavMenu.razor.g.cs
@@ -13,70 +13,70 @@ namespace DieRoller.Client.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\Cleuseau\Source\Repos\Kevonh\DragonDice\DieRoller\Client\_Imports.razor"
+#line 1 "C:\Users\khoughton\Source\Repos\kevonh\DragonDice\DieRoller\Client\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\Cleuseau\Source\Repos\Kevonh\DragonDice\DieRoller\Client\_Imports.razor"
+#line 2 "C:\Users\khoughton\Source\Repos\kevonh\DragonDice\DieRoller\Client\_Imports.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\Cleuseau\Source\Repos\Kevonh\DragonDice\DieRoller\Client\_Imports.razor"
+#line 3 "C:\Users\khoughton\Source\Repos\kevonh\DragonDice\DieRoller\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\Cleuseau\Source\Repos\Kevonh\DragonDice\DieRoller\Client\_Imports.razor"
+#line 4 "C:\Users\khoughton\Source\Repos\kevonh\DragonDice\DieRoller\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\Cleuseau\Source\Repos\Kevonh\DragonDice\DieRoller\Client\_Imports.razor"
+#line 5 "C:\Users\khoughton\Source\Repos\kevonh\DragonDice\DieRoller\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\Cleuseau\Source\Repos\Kevonh\DragonDice\DieRoller\Client\_Imports.razor"
+#line 6 "C:\Users\khoughton\Source\Repos\kevonh\DragonDice\DieRoller\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\Cleuseau\Source\Repos\Kevonh\DragonDice\DieRoller\Client\_Imports.razor"
+#line 7 "C:\Users\khoughton\Source\Repos\kevonh\DragonDice\DieRoller\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\Cleuseau\Source\Repos\Kevonh\DragonDice\DieRoller\Client\_Imports.razor"
+#line 8 "C:\Users\khoughton\Source\Repos\kevonh\DragonDice\DieRoller\Client\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\Cleuseau\Source\Repos\Kevonh\DragonDice\DieRoller\Client\_Imports.razor"
+#line 9 "C:\Users\khoughton\Source\Repos\kevonh\DragonDice\DieRoller\Client\_Imports.razor"
 using DieRoller.Client;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\Cleuseau\Source\Repos\Kevonh\DragonDice\DieRoller\Client\_Imports.razor"
+#line 10 "C:\Users\khoughton\Source\Repos\kevonh\DragonDice\DieRoller\Client\_Imports.razor"
 using DieRoller.Client.Shared;
 
 #line default
@@ -90,7 +90,7 @@ using DieRoller.Client.Shared;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 28 "C:\Users\Cleuseau\Source\Repos\Kevonh\DragonDice\DieRoller\Client\Shared\NavMenu.razor"
+#line 28 "C:\Users\khoughton\Source\Repos\kevonh\DragonDice\DieRoller\Client\Shared\NavMenu.razor"
        
     private bool collapseNavMenu = true;
 
